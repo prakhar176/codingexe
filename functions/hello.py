7a44de35af352bee4228e8ab5480bd9c7ec6cfef
prakhar176 <104298317+prakhar176@users.noreply.github.com>
2022-06-09 19:32:10 +0530

    Push

---
+++ functions/hello.py
@@ -0,0 +1,10 @@
+import random
+
+def say_hello():
+    msg = ['Hello','Namaste','Hola','Bonjour','Guten Tag']
+    print(random.choice(msg))
+
+
+say_hello()
+say_hello()
+say_hello()
---
+++ functions/intro.py
@@ -0,0 +1,12 @@
+
+def hypotenuse():
+    p = float(input("enter the length of thefirst side"))
+    b = float(input("enter the lenghth of the second side"))    
+
+    h = (p**2 + b**2)**0.5
+    print('perpendicular: ', p)
+    print('base: ',b)
+    print("the hypotenuse is: ", h)
+##calling
+
+hypotenuse()    
